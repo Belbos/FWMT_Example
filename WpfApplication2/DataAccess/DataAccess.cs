612e1d6c66bd8948f2731739702add678b77359a
jongyoung <maplelyy@naver.com>
2019-11-25 17:05:31 +0900

    Xml Read/Write 를 통한 ConnectionString 불러오기 (작업중)  1) Xml 파일을 못 읽으면 내컴퓨터/문서 파일에 추가 (완료)  2) XMl 파일을 못 읽으면 내컴퓨터/문서 파일에 파일 존재여부 확인(완료)  3) Xml 파일 읽으면 그 파일로 ConnectionString 가져오기 (작성중)

--- WpfApplication2/DataAccess/DataAccess.cs
+++ WpfApplication2/DataAccess/DataAccess.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,9 @@ public class DataAccess
 
     public static SQLiteConnection SqlConnection()
     {
-        Console.WriteLine("XqlPassing " + XqlPassing());
-        string connStr = @"Data Source=C:\Users\jongyoung\Documents\Visual Studio 2015\Projects\WpfApplication2\sqlite\sqlite;Pooling=true;FailIfMissing=false";
+         Console.WriteLine("XqlPassing " + ReadConnectionStringXml());
+        WriteConnectionStringXml();
+        string connStr = "Data Source=" + ReadConnectionStringXml() + ";Pooling=true;FailIfMissing=false";
         SQLiteConnection sqliteConnection = new SQLiteConnection(connStr);
    
         sqliteConnection.Open();
@@ -21,24 +23,57 @@ public class DataAccess
         return sqliteConnection;
     }
 
-
-    private static String XqlPassing()
+    #region xml읽어서 ConnectionString 가져옴
+    private static String ReadConnectionStringXml()
     {
         string returnXml = "";
         XmlDocument xml = new XmlDocument();
-        xml.Load(@"Setting\ConnectionString.xml"); //"D:\\test\\config.xml" == @"D:\test\config.xml" 
+        string path = AppDomain.CurrentDomain.BaseDirectory + @"\Setting";
+       
+
+        xml.Load(path  + @"\ConnectionString.xml"); //"D:\\test\\config.xml" == @"D:\test\config.xml" 
         XmlNodeList xmlList = xml.SelectNodes("/CONNECTION");
         foreach (XmlNode xnl in xmlList)
         {
             returnXml = xnl["STRING"].InnerText;
-
+     
         }
-
-
-
         return returnXml;
+    }
+    #endregion
+
+    #region xml 파일이 없으면 Desktop 문서에 저장
+    private static String WriteConnectionStringXml()
+    {
+        XmlDocument xml = new XmlDocument();
+        // xml 자동생성 경로
+        String path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\FireWillManagementTool\Setting";
+
+        // 메인 XMl root
+        XmlNode root = xml.CreateElement("MAIN");
+        XmlNode CONNECTION = xml.CreateElement("CONNECTION");
+        XmlNode STRING = xml.CreateElement("STRING");
+
+        // Value 값 입력
+        STRING.InnerText = path;
+
+        CONNECTION.AppendChild(STRING);
+        root.AppendChild(CONNECTION);
+        xml.AppendChild(root);
+
+        // 폴더에 경로 존재여부 (없으면 폴더 생성)
+        if ( !( Directory.Exists(path) ) )
+        {
+            Directory.CreateDirectory(path);
+        }
+        xml.Save(path + @"\ConnectionString.xml");
+
+        //DirectoryInfo dir = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)+ @"\Setting");
+
+        return path;
 
     }
+    #endregion
 
-     
+
 }
