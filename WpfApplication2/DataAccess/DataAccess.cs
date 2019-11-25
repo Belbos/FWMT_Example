68248f0a03d5892c70eb49edf67ba7ed87cc05c5
jongyoung <jongyoung@DESKTOP-OQLEKR9>
2019-11-25 15:56:10 +0900

    초기 커밋.

---
+++ WpfApplication2/DataAccess/DataAccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+public class DataAccess
+{
+
+    public static SQLiteConnection SqlConnection()
+    {
+        Console.WriteLine("XqlPassing " + XqlPassing());
+        string connStr = @"Data Source=C:\Users\jongyoung\Documents\Visual Studio 2015\Projects\WpfApplication2\sqlite\sqlite;Pooling=true;FailIfMissing=false";
+        SQLiteConnection sqliteConnection = new SQLiteConnection(connStr);
+   
+        sqliteConnection.Open();
+
+        return sqliteConnection;
+    }
+
+
+    private static String XqlPassing()
+    {
+        string returnXml = "";
+        XmlDocument xml = new XmlDocument();
+        xml.Load(@"Setting\ConnectionString.xml"); //"D:\\test\\config.xml" == @"D:\test\config.xml" 
+        XmlNodeList xmlList = xml.SelectNodes("/CONNECTION");
+        foreach (XmlNode xnl in xmlList)
+        {
+            returnXml = xnl["STRING"].InnerText;
+
+        }
+
+        return returnXml;
+
+
+    }
+
+     
+}
---
+++ WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            label1.Content = DataSelect(textBox.Text);
+        }
+
+        public string DataSelect(string str)
+        {
+            String resual = "";
+
+            SQLiteDataReader sqliteDataReader = null;
+
+            try
+            {
+
+                SQLiteConnection sqliteConnection = DataAccess.SqlConnection();             
+
+                string sql = "SELECT * FROM tmptable where id = @id";
+                SQLiteCommand sqliteCommand = new SQLiteCommand(sql, sqliteConnection);
+
+                sqliteCommand.CommandType = CommandType.Text;
+                sqliteCommand.Parameters.AddWithValue("@id", str); 
+                      
+           
+                sqliteDataReader = sqliteCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                if (sqliteDataReader.Read())
+                {
+                    resual = sqliteDataReader["test"].ToString();
+                }
+            }
+            finally
+            {
+                sqliteDataReader.Close();
+            }
+
+            return resual;
+        }
+    }
+}
